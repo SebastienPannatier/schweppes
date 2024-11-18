f9b47030450528ba7708a2a933bb65774791ebdf
SebastienPannatier <36041410+SebastienPannatier@users.noreply.github.com>
2024-11-18 11:53:17 +0100

    feat(Hero): Default Hero

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "@/app/img/logo.png";
+import circular_text from "@/app/img/Circular-Text.png";
+import scrollImg from "@/app/img/scroll.png";
+import "@/app/styles/hero.css";
+import localFont from "next/font/local";
+
+const monumentExtended = localFont({
+  src: "../fonts/MonumentExtended-Regular.otf",
+});
+
+export const Hero = () => {
+  return (
+    <section className="h-screen w-full pl-10 pt-5 flex">
+      <img src={logo.src} alt={logo.src} className="w-64 absolute" />
+      <img
+        src={circular_text.src}
+        alt={circular_text.src}
+        className="w-48 absolute right-0 mr-28 top-0 mt-20 rotateLeft"
+      />
+      <div className="flex w-full">
+        <h1
+          className={
+            (monumentExtended.className,
+            "font-extrabold m-auto text-9xl text-center tracking-wide titre")
+          }
+        >
+          NEW AND FRESH FLAVOURS
+        </h1>
+      </div>
+      <img
+        src={scrollImg.src}
+        alt={scrollImg.src}
+        className="absolute bottom-0 left-0 ml-28 mb-20 rotateRight"
+      />
+    </section>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import { Hero } from "@/app/components/Hero";
+import "@/app/styles/global.css";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
