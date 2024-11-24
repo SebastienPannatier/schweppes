caac8f3ac2c90488f344faba1ea1a045d2acb0b9
SebastienPannatier <36041410+SebastienPannatier@users.noreply.github.com>
2024-11-24 18:17:37 +0100

    feat(Classic product animation): Become bigger when mouse hover

--- src/app/components/elements/ClassicProduct.jsx
+++ src/app/components/elements/ClassicProduct.jsx
@@ -1,11 +1,44 @@
-import React from "react";
+"use client";
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
 
 export const ClassicProduct = ({ productImage, bgColor }) => {
+  const productRef = useRef(null);
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const element = productRef.current;
+    const image = imageRef.current;
+
+    gsap.set(image, { scale: 1 });
+
+    const handleMouseEnter = () => {
+      gsap.to(image, { scale: 1.1, duration: 0.1 });
+    };
+
+    const handleMouseLeave = () => {
+      gsap.to(image, { scale: 1, duration: 0.1 });
+    };
+
+    element.addEventListener("mouseenter", handleMouseEnter);
+    element.addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      element.removeEventListener("mouseenter", handleMouseEnter);
+      element.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
+
   return (
     <div
+      ref={productRef}
       className={`${bgColor} w-[25%] aspect-auto rounded-3xl flex justify-center items-center flex-shrink-0`}
     >
-      <img src={productImage.src} className="h-[80%]" />
+      <img
+        ref={imageRef}
+        src={productImage.src}
+        className="h-[80%] transition-transform duration-300"
+      />
     </div>
   );
 };
